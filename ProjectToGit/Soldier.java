a5b5aec1c621cd09eca6b5e242e61484675129e7
Your Name <you@example.com>
2023-05-25 23:16:12 -0600

    Avance Viernes

--- ProjectToGit/Soldier.java
+++ ProjectToGit/Soldier.java
@@ -2,23 +2,54 @@ import greenfoot.*;
 
 public class Soldier extends Person
 {
-    private int velocidad = 5; // Velocidad de movimiento del objeto
+    private int velocidad; // Velocidad de movimiento del objeto
+    private int velocidadGravedad;
+    private int piso;
+    private boolean space;
+    GreenfootImage right1;
+    GreenfootImage right2;
+    GreenfootImage left1;
+    GreenfootImage left2;
     
-    public void act()
+    public Soldier()
     {
-        move();
-        setImage("Soldier1.png");
+        velocidad = 3;
+        velocidadGravedad = 7;
+        piso = 434;
+        space = false;
+        right1 = new GreenfootImage("0.png");
+        right2 = new GreenfootImage("1.png");
+        left1 = new GreenfootImage("3.png");
+        left2 = new GreenfootImage("2.png");
     }
-
     public void move()
     {
         if (Greenfoot.isKeyDown("left")) {
             setLocation(getX() - velocidad, getY()); // Mover hacia la izquierda
+            setImage(left1);
         } else if (Greenfoot.isKeyDown("right")) {
             setLocation(getX() + velocidad, getY()); // Mover hacia la derecha
+            setImage(right1);
         }
-        if (Greenfoot.isKeyDown("space")){
-            setLocation(getY() + velocidad, getX());
+         if (Greenfoot.isKeyDown("space") && !space) {
+            setLocation(getX(), getY() - 100); // Salto
+            space = true; // Establecer la variable de control a verdadero
         }
+        if (!Greenfoot.isKeyDown("space")) {
+            space = false; // Restablecer la variable de control cuando se suelta la tecla
+        }
+    }
+    
+    public void gravedad()
+    {
+        if(getY()<piso)
+        {
+            setLocation(getX(), getY() + velocidadGravedad);
+        }
+    }
+    public void act()
+    {
+        move();
+        gravedad();
     }
 }
